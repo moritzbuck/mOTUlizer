5ba26df3468b0e54937eb771df7f72ea37d13e3f
moritzbuck <mrtz.buck@gmail.com>
2019-11-23 09:05:43 +0100

    fixed so different versions of diamond don't spit out tons of info

--- post_proc_scripts/run_gtdb.py
+++ post_proc_scripts/run_gtdb.py
@@ -3,6 +3,9 @@ import pandas
 from tqdm import tqdm
 from os.path import join as pjoin
 import sys
+import random
+import json
+
 sys.path.append("/home/moritz/repos/moritz/0039_mOTUlizer/")
 from mOTUlizer.classes.mOTU import mOTU
 
@@ -14,7 +17,7 @@ md = pandas.concat([pandas.read_csv(gtdb_bac_md_file, sep="\t"),pandas.read_csv(
 dd = { v[1]['accession'] : v[1]['gtdb_genome_representative']  for v in md.iterrows()}
 mOTU2genome =  {d : []  for d in set(dd.values())}
 checkm = { v[1]['accession'] : v[1]['checkm_completeness']  for v in md.iterrows()}
-
+md.index = md.accession
 
 for k,v in tqdm(dd.items()):
     mOTU2genome[v] += [k]
@@ -27,13 +30,37 @@ def make_file(genome_id) :
     return path
 
 mOTU2aa = {k : {g : make_file(g) for g in v} for k,v in mOTU2genome.items() if len(v) > 5}
-mOTU2aa = {k : {g : p for g, p in v.items() if os.path.exists(p)} for k,v in tqdm(list(mOTU2aa.items())[0:10])}
+mOTU2aa = {k : {g : p for g, p in v.items() if os.path.exists(p)} for k,v in tqdm(list(mOTU2aa.items()))}
 mOTU2aa = {k : v for k,v in tqdm(mOTU2aa.items()) if len(v) > 5 }
 for k,v in tqdm(mOTU2aa.items()):
      if len(v) > 50:
          selected = random.sample(list(v.items()),50)
          mOTU2aa[k] = dict(selected)
 
+out_dat = {}
+with open("gtdb_cores.json") as handle:
+    out_dat = json.load(handle)
 
-for k, v in mOTU2aa:
-    motu = mOTU( k , v , cog_dict, checkm_dict = {v : checkm [v]for vv in v.keys()})
+for k, v in tqdm(mOTU2aa.items()):
+    if k not in out_dat:
+        motu = mOTU( k , v , None, checkm_dict = {vv : checkm [vv] for vv in v.keys()})
+        out_dat.update(motu.get_stats())
+        with open("gtdb_cores.json", "w") as handle :
+            json.dump(out_dat, handle)
+
+cool_dat =  pandas.DataFrame.from_dict(
+    { k :
+        {
+        "mean_cogs" : mean([len(l) for l in  v['cogs']['genome'].values()]),
+        "core_len" : len(v['core']),
+        "aux_len" : len(v['aux_genome']),
+        "aux_sinsingle" : len(set(v['aux_genome']).difference(v['singleton_cogs'])),
+        "nb_genomes" : len(v['cogs']['genome'])
+        }
+        for k,v in out_dat.items()
+    }, orient="index")
+
+taxo = pandas.DataFrame.from_dict({k : {l : vv[3:] for l,vv in zip(["domain", "phylum", "class", "order", "family", "genus", "species"] , v.split(";"))}  for k,v in md.loc[cool_dat.index]['gtdb_taxonomy'].iteritems()}, orient="index")
+
+cool_dat = pandas.concat([cool_dat, taxo], axis = 1)
+cool_dat.to_csv("cool_dat.csv")
