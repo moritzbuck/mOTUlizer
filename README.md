98c35f97094243f648c4f77aafa69bfbd3d98586
moritzbuck <moritz.buck@slu.se>
2020-07-16 10:27:27 +0200

    pickup  some from master

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 0039_mOTUlizer
+# mOTUlizer
 Utility to analyse a group of closely related MAgs/Genomes/bins/SUBs of more or less dubious origin
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mOTUlizer", # Replace with your own username
+    version="0.0.1",
+    author="Moritz Buck",
+    author_email="moritz.buck@slu.se",
+    description="making OTUs from genomes, and stats on them. and maybe even core-genomes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moritzbuck/0039_mOTUlizer/tree/dev",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    keywords='bioinformatics clustering metagenomics microbial-genomics genomics',
+    python_requires='>=3.6',
+    scripts = ['mOTUlizer/bin/mOTUlize.py','mOTUlizer/bin/mOTUpan.py']
+
+)
