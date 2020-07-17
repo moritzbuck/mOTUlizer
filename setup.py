51446834eecb1fc396b2974470beb98d092e209a
moritzbuck <moritz.buck@slu.se>
2020-07-17 10:35:37 +0200

    updated setup to reflect version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mOTUlizer", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Moritz Buck",
     author_email="moritz.buck@slu.se",
     description="making OTUs from genomes, and stats on them. and maybe even core-genomes",
