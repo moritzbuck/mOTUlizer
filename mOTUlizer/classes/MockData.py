954f43712610b615aeb806f04ee0fb0a0cb33dee
moritzbuck <moritz.buck@slu.se>
2020-08-24 18:24:13 +0200

    arrrrrrrrr

--- mOTUlizer/classes/MockData.py
+++ mOTUlizer/classes/MockData.py
@@ -1,45 +1,54 @@
-from random import random
+from random import random, sample
 from mOTUlizer.classes.mOTU import mOTU, mean
 from collections import defaultdict
 from tqdm import tqdm
 from numpy.random import normal
+from numpy import floor
 
 genome2guass = {}
 
+with open("mOTUlizer/data/Efeacalis.csv") as handle:
+    emp_disp = [int(l[:-1].split(",")[-1])/691 for l in handle if "accessory" in l]
+gene_count = sum(emp_disp)
 
 class MockmOTU(mOTU):
     def __repr__(self) :
         return "< MockmOTU with {n} genomes, of average {c}% completness, with core/genome_len of {r} >".format(c = 100*self.mean_completeness, n = len(self), r = self.ratio)
 
-    def __init__(self, name, ratio, genome_len, nb_genomes, accessory_len, completeness):
+    def __init__(self, name, ratio, genome_len, nb_genomes, completeness):
 
         self.ratio = ratio
         core_len = int(genome_len*self.ratio)
         core = {"CoreTrait_{}".format(i) for i in range(core_len)}
+        weird_dist = [round(e*nb_genomes) for e in emp_disp]
+        nb_variable_genes = int(genome_len*(1-ratio))*nb_genomes
+        sub_dist = [nb_variable_genes +1]
+        i=1
+        while sum(sub_dist) > nb_variable_genes:
+            sub_dist = [weird_dist[k] for k in range(0,len(weird_dist), i) if weird_dist[k] > 0]
+            i += 1
+        if i >2:
+            sub_dist = [weird_dist[k] for k in range(0,len(weird_dist), i-2) if weird_dist[k] > 0]
+        else:
+            sub_dist = [weird_dist[k] for k in range(0,len(weird_dist), i-1) if weird_dist[k] > 0]
 
-        rank2prob = lambda i: 1/(i+1)+0.01
-        accessory = [rank2prob(i) for i in range(accessory_len)]
+        tests = [ sub_dist[:i] for i in range(len(sub_dist)) if sum(sub_dist[:i]) > nb_variable_genes]
+        sub_dists = tests[0]
 
         mock_genomes = dict()
+        for k in range(nb_genomes):
+            mock_genomes["Genome_{}".format(k)] = list(core)
 
+        for i,v in enumerate(sub_dist):
+            genomes = sample(list(mock_genomes.keys()), v)
+            for g in genomes:
+                mock_genomes[g] += ["AccessoryTrait_{}".format(i)]
 
-        for k in tqdm(range(nb_genomes)):
-            variable = []
-            i = 1
-            while(len(variable) < (genome_len*(1-ratio))):
-                if (len(accessory)-1) < i :
-                    variable += ["AccessoryTrait_{}".format(i)]
-                    accessory += [rank2prob(i)]
-                else :
-                    if random() < accessory[i]:
-                        variable += ["AccessoryTrait_{}".format(i)]
-                i += 1
-            mock_genomes["Genome_{}".format(k)] = core.union(variable)
-
-            self.incompletes = {g : {vv for vv in v if random() < (completeness(g)/100)} for g, v in mock_genomes.items()}
-            self.mean_completeness = mean([len({vv for vv in v if vv.startswith("CoreTrait_")})/core_len for c,v in self.incompletes.items()])
-        self.accessory = accessory
-        super().__init__(name = name, faas = {}, cog_dict = self.incompletes, checkm_dict = {g : 95 for g in self.incompletes}, max_it = 50)
+        self.incompletes = {g : {vv for vv in v if random() < (completeness(g)/100)} for g, v in mock_genomes.items()}
+        self.mean_completeness = mean([len({vv for vv in v if vv.startswith("CoreTrait_")})/core_len for c,v in self.incompletes.items()])
+#        self.accessory = accessory
+        self.mean_size = mean([len(m) for m in mock_genomes.values()])
+        super().__init__(name = name, faas = {}, cog_dict = self.incompletes, checkm_dict = "length_seed", max_it = 50)
 
 
     @classmethod
