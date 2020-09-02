8e636d6e2f265831b681a9c36b43f22b65c23313
moritzbuck <moritz.buck@slu.se>
2020-09-02 19:53:26 +0200

    nothin

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="making OTUs from genomes, and stats on them. and maybe even core-genomes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/moritzbuck/0039_mOTUlizer/tree/dev",
+    url="https://github.com/moritzbuck/0039_mOTUlizer/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
