4ed14f596f0dcc4e119991c48e9d5f0545271bc4
moritzbuck <moritz.buck@slu.se>
2021-06-23 12:18:41 +0200

    updated version tag

--- mOTUlizer/__init__.py
+++ mOTUlizer/__init__.py
@@ -10,7 +10,7 @@ os.chdir(module_path)
 if os.path.exists(".git"):
     label = subprocess.check_output(["git", "describe", "--tags"]).strip().decode()
 else:
-    label = "0.2.4"
+    label = "0.2.5"
 
 os.chdir(cwd)
 
